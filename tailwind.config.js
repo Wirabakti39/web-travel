66b541a1f921cb2fbd5343d3017e14a279913277
Wirabakti39 <wirabakti022@gmail.com>
2024-07-08 14:19:53 +0800

    section travel point done

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
         "pink" : '#F85E9F',
         "orange" : '#FF5722',
         "abu" : '#222831'
+      },
+      dropShadow : {
+        "sm-shadow" : [
+          '0px 548px 219px rgba(0,0,0, 0.01)',
+          '0px 308px 185px rgba(0,0,0, 0.04)',
+          '0px 34px 75px rgba(0,0,0, 0.07)',
+          '0px 0px 0px rgba(0,0,0, 0.07)'
+        ]
       }
     },
   },
